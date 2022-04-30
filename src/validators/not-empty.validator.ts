a02722aa8a98f8128050a28aee7dc99db37f865e
Nico Loos <nico.loos@werkdigital.de>
2022-04-30 15:06:11 +0200

    Not empty error message

--- src/validators/not-empty.validator.ts
+++ src/validators/not-empty.validator.ts
@@ -1,3 +1,7 @@
-export const notEmpty = (x: string | any[]): boolean => {
-  return x.length !== 0;
+export namespace NotEmpty {
+  export const validator = (x: string | any[]): boolean => {
+    return x.length !== 0;
+  };
+
+  export const errorMessage = "String or array must not be empty.";
 }
--- test/validators/not-empty-validator.test.ts
+++ test/validators/not-empty-validator.test.ts
@@ -1,20 +1,24 @@
 import { equal } from 'assert';
-import { notEmpty } from '../../src/validators/not-empty.validator';
+import { NotEmpty } from '../../src/validators/not-empty.validator';
 
 describe('NotEmpty test suite', () => {
-  it('Should return false on an empty string', () => {
-    equal(notEmpty(''), false);
+  it('Validator should return false on an empty string', () => {
+    equal(NotEmpty.validator(''), false);
   });
 
-  it('Should return false on an empty array', () => {
-    equal(notEmpty([]), false);
+  it('Validator should return false on an empty array', () => {
+    equal(NotEmpty.validator([]), false);
   });
 
-  it('Should return true on a non-empty string', () => {
-    equal(notEmpty(' '), true);
+  it('Validator should return true on a non-empty string', () => {
+    equal(NotEmpty.validator(' '), true);
   });
 
-  it('Should return true on a non-empty array', () => {
-    equal(notEmpty([1]), true);
+  it('Validator should return true on a non-empty array', () => {
+    equal(NotEmpty.validator([1]), true);
+  });
+
+  it('ErrorMessage should return specific string', () => {
+    equal('String or array must not be empty.', NotEmpty.errorMessage);
   });
 });
