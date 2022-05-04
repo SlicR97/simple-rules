38f334fadef4edf9423c76923af701a3325e8550
Nico Loos <nico.loos@werkdigital.de>
2022-05-04 08:49:59 +0200

    Validate a nested object

--- src/rules.ts
+++ src/rules.ts
@@ -2,11 +2,34 @@ import { Maybe } from "./types/maybe.type";
 import { RuleViolation, RuleViolations } from "./types/rule-violation.type";
 import { ValidationResult } from './types/validation-result.type';
 
-export type RuleFunction<T, TU extends keyof T> = (x: T[TU], propertyName: TU) => Maybe<RuleViolation>;
-export type Rule<T, TU extends keyof T> = [TU, ...(RuleFunction<T, TU>)[]];
+type NormalRuleFunction<T, TU extends keyof T> = (x: T[TU], propertyName: TU) => Maybe<RuleViolation>;
+type NormalRule<T, TU extends keyof T> = [TU, ...(NormalRuleFunction<T, TU>)[]];
 
-export const rules = <T>(...rules: Rule<T, keyof T>[]) => (t: T): ValidationResult<T> => rules.reduce((res, rule) => {
-  const [key, ...fns] = rule;
+type NestedRuleFunction<T, TU extends keyof T> = (t: T[TU]) => ValidationResult<T[TU]>;
+type NestedRule<T, TU extends keyof T> = [TU, NestedRuleFunction<T, TU>];
+
+type Rule<T, TU extends keyof T> = NormalRule<T, TU> | NestedRule<T, TU>;
+
+const _validateNestedRule = <T, TU extends keyof T>(
+  res: ValidationResult<T>, 
+  fn: NestedRuleFunction<T, TU>, 
+  t: T, 
+  key: TU
+): ValidationResult<T> => {
+  const nestedRuleViolations = fn(t[key]);
+  if (Object.keys(nestedRuleViolations).length) {
+    return ValidationResult.apply(res, key, nestedRuleViolations);
+  } else {
+    return res;
+  }
+}
+
+const _validateNormalRule = <T, TU extends keyof T>(
+  res: ValidationResult<T>,
+  fns: NormalRuleFunction<T, TU>[],
+  t: T,
+  key: TU
+): ValidationResult<T> => {
   const ruleViolations = fns
     .map(x => x(t[key], key))
     .filter(x => !!x) as RuleViolations;
@@ -15,5 +38,14 @@ export const rules = <T>(...rules: Rule<T, keyof T>[]) => (t: T): ValidationResu
     return ValidationResult.apply(res, key, ruleViolations);
   } else {
     return res;
+  }
+}
+
+export const rules = <T>(...rules: Rule<T, keyof T>[]) => (t: T): ValidationResult<T> => rules.reduce((res, rule) => {
+  const [key, ...fns] = rule;
+  if (fns[0] && fns[0].length === 1) {
+    return _validateNestedRule(res, fns[0] as NestedRuleFunction<T, keyof T>, t, key);
+  } else {
+    return _validateNormalRule(res, fns as NormalRuleFunction<T, keyof T>[], t, key);
   }
 }, ValidationResult.empty<T>());
