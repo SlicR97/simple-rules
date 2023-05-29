f770d2c88f626992a32848841277bcb9f656147f
Nico Loos <nico.loos@slicr.de>
2023-05-29 11:40:42 +0200

    SIM-35: Length Rule

---
+++ src/rules/length.rule.ts
@@ -0,0 +1,52 @@
+import { Maybe, RuleViolation, basicRule } from '../index'
+
+/**
+ * Validation for checking
+ * if the property has a certain length
+ * (string or array)
+ */
+export namespace Length {
+  /**
+   * Options for configuring the Length rule
+   *
+   * @param other Value to compare against
+   */
+  export type Options = {
+    length: number
+  }
+
+  /**
+   * Checks if input's length is equal to the supplied property
+   *
+   * @param x Input to be checked
+   * @param length Expected length
+   * @returns True if the input's is equal to the expected value
+   */
+  export const rule = <T extends string | unknown[]>(
+    x: T,
+    length: number,
+  ): boolean => {
+    return x.length === length
+  }
+
+  /**
+   * Partially applied function.
+   *
+   * @param options Validation options
+   * @returns a function that takes a value to validate
+   */
+  export const validate =
+    <T extends string | unknown[]>(options: Options) =>
+    /**
+     * Validation function
+     *
+     * @param x Input for validation
+     * @returns RuleViolation if the validation fails, or nothing if it succeeds
+     */
+    (x: T): Maybe<RuleViolation<T>> =>
+      basicRule({
+        ruleSatisfied: rule(x, options.length),
+        errorCode: 'LENGTH',
+        propertyValue: x,
+      })
+}
